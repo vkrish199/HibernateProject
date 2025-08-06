af6a73edea0af205163b300b75e4ef21f105a66c
Varun K <varun.krishnamurthy@yahoo.com>
2025-08-06 05:38:42 +0530

    first attempt to save data

--- README.md
+++ README.md
@@ -3,4 +3,17 @@
 * Hibernate is basically an ORM (Object relational mapping)
 * In hibernate every row in a table corresponds to an object in java
 * Every column refers to an attribute/variable in the class
-* 
+
+
+* We normally add hibernate configuration in an xml file
+* refer hibernate.cfg.xml
+* refer Main.java to see how to connect
+    1. Load Configuration
+    2. Build session factory
+    3. Open the session
+    4. Begin transaction 
+    5. Persist 
+    6. Commit transcation 
+* We can use @Entity annotation provide by Java persistence API
+* We use JPA annotations
+* @Id is needed on atleast one column to indicate a primary key
--- src/main/java/org/varun/Main.java
+++ src/main/java/org/varun/Main.java
@@ -1,9 +1,30 @@
 package org.varun;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        Student s1 = new Student();
+
+        s1.setsName("Varun");
+        s1.setRollNo(101);
+        s1.setsAge(33);
+
+        Configuration cfg = new Configuration();
+        cfg.addAnnotatedClass(org.varun.Student.class);
+        cfg.configure();
+        //Use session factory only once as it is heavyweight
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tr = session.beginTransaction();
+        session.persist(s1);
+        tr.commit();
+
+        System.out.println(s1);
 
     }
 }
