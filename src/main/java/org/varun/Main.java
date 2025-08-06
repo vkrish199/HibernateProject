5580725e9f10e6a50a366dd8c984cdcc702b80ee
Varun K <varun.krishnamurthy@yahoo.com>
2025-08-06 06:05:49 +0530

    refactor

--- src/main/java/org/varun/Main.java
+++ src/main/java/org/varun/Main.java
@@ -8,23 +8,37 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
 
+        //Use session factory only once as it is heavyweight
+        SessionFactory sf = new Configuration()
+                .addAnnotatedClass(org.varun.Student.class)
+                .configure()
+                .buildSessionFactory();
+        Session session = sf.openSession();
+//        new Main().createData(session);
+        new Main().fetchData(session);
+        sf.close();
+    }
+
+    public void createData(Session session) {
+        //only needed when we are persisting
+        Transaction tr = session.beginTransaction();
         Student s1 = new Student();
 
-        s1.setsName("Rama");
-        s1.setRollNo(105);
-        s1.setsAge(84);
-
-        Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(org.varun.Student.class);
-        cfg.configure();
-        //Use session factory only once as it is heavyweight
-        SessionFactory sf = cfg.buildSessionFactory();
-        Session session = sf.openSession();
-        Transaction tr = session.beginTransaction();
+        s1.setsName("Anu");
+        s1.setRollNo(107);
+        s1.setsAge(65);
+        //we had save before which is now deprecated in favour of persist
         session.persist(s1);
         tr.commit();
+        session.close();
 
         System.out.println(s1);
+    }
 
+    public void fetchData(Session session) {
+        Student s2 = null;
+        //we had get before which is now deprecated in favour of find
+        s2 = session.find(Student.class, 107);
+        System.out.println(s2);
     }
 }
